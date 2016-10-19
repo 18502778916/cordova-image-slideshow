3131ddb7a64061d17ca1dbf060c2e12cf18f45bd
张冲 <330686058@qq.com>
2016-10-19 11:35:17 +0800

    错误修改

--- src/android/ImageShowActivity.java
+++ src/android/ImageShowActivity.java
@@ -32,7 +32,7 @@ public class ImageShowActivity extends Activity {
         setContentView(R.layout.image_show_activity);
 
         init();
-        showImage(listUrl,imageNum);
+        showImage(listUrl,imageNum，type);
 
     }
 
